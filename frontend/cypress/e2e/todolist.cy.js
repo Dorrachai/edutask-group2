ce73080d14fa8d94f64f5bc7449729e4db662554
Dorrachai <93660087+Dorrachai@users.noreply.github.com>
2023-07-05 21:10:04 +0100

    Update todolist.cy.js

--- frontend/cypress/e2e/todolist.cy.js
+++ frontend/cypress/e2e/todolist.cy.js
@@ -17,6 +17,45 @@ describe("R8UC1: Create To-Do Items", () => {
     cy.get(".container-element a").last().click();
   });
 
+  it("TC1.1: Enter a valid text in the input field and click on the submit button", () => {
+    cy.get(".inline-form").within(() => {
+      cy.get("input[type=text]").type("Task 1");
+      cy.get("input[type=submit]").click();
+    });
+
+    cy.get("li.todo-item").last().contains("span", "Task 1");
+  });
+
+  it("TC1.2: Enter a minimum length (empty) text and click on the submi tbutton", () => {
+    cy.get(".inline-form").within(() => {
+      cy.get("input[type=submit]").should("be.disabled");
+    });
+  });
+
+  it("TC1.3: Enter a maximum length text and click on the submit button", () => {
+    cy.get(".inline-form").within(() => {
+      cy.get("input[type=text]").type(MAX_LENGTH_TEXT);
+      cy.get("input[type=submit]").click();
+    });
+
+    cy.get("li.todo-item").last().contains("span", MAX_LENGTH_TEXT);
+  });
+});
+
+// Test suite for toggling the completion status of a to-do item
+describe("R8UC2: Toggle the completion status of a to-do item", () => {
+  // Log in before each test case
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+    cy.viewport(1920, 1080);
+    cy.contains("div", "Email Address")
+      .find("input[type=text]")
+      .type("mon.doe@gmail.com");
+
+    cy.get("form").submit();
+    cy.get(".container-element a").last().click();
+  });
+
   it("TC2.1: Click on the checkbox of an incomplete to-do item", () => {
     cy.get("li.todo-item").last().within(() => {
       cy.get("span").first().click(); // Click on the first span (when it's unchecked)
